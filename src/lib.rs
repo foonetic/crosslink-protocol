d6a4c2a6157e6fafdb773113a94386ec8efb6b88
Chao Xu <fardream@users.noreply.github.com>
2022-04-27 00:13:54 +0000

    remove one layer of mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,20 @@
-pub mod crosslink {
-    tonic::include_proto!("crosslink");
-}
+tonic::include_proto!("crosslink");
 
 #[cfg(test)]
 mod tests {
-    use super::crosslink;
+    use super::*;
     use prost::Message;
 
     #[test]
     fn construct_proto() {
-        let decimal = crosslink::DecimalValue {
+        let decimal = DecimalValue {
             value: 1,
             decimal: 2,
         };
         let mut encoded = Vec::new();
         encoded.reserve(decimal.encoded_len());
         decimal.encode(&mut encoded).unwrap();
-        let decoded = crosslink::DecimalValue::decode(std::io::Cursor::new(encoded)).unwrap();
+        let decoded = DecimalValue::decode(std::io::Cursor::new(encoded)).unwrap();
 
         assert_eq!(decoded.value, 1);
         assert_eq!(decoded.decimal, 2);
